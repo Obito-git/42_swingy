1be9ba03f79c0e82a216af5b7b1eed4825a67ea9
amyroshn <antonmyroshnychenko@gmail.com>
2023-01-04 14:10:51 +0100

    Docker update.

--- postgres-docker/delete.sh
+++
@@ -1,6 +0,0 @@
-docker-compose stop
-docker-compose down
-docker system prune -f
-docker image prune -f
-docker rmi -f $(docker images -a -q)
-rm -rf ~/goinfre/database
--- postgres-docker/run_docker.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/goinfre/database
-docker-compose up -d
-docker exec -it postgres sh -c "chmod -R 777 /var/lib/postgresql/data/"
-
